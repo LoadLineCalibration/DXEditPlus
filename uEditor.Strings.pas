154f3cf9e082e23b0adc1eb40936f689f2a18e24
LoadLine Calibration <Veronika-and@yandex.ru>
2024-08-14 18:03:20 +0900

    Strings

---
+++ uEditor.Strings.pas
@@ -0,0 +1,11 @@
+unit uEditor.Strings;
+
+interface
+
+resourcestring
+    strAskBuildPaths = 'This command will erase all existing pathnodes and attempt to create a pathnode network on its own.' +
+    '  Are you sure this is what you want to do?' + #13#10#13#10 + 'NOTE : This process can take a VERY long time.';
+
+implementation
+
+end.
