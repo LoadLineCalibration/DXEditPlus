388aa438e365745433dc597fe230b2f441313787
LoadLine Calibration <Veronika-and@yandex.ru>
2024-08-17 13:26:33 +0900

    New strings

--- uEditor.Strings.pas
+++ uEditor.Strings.pas
@@ -6,6 +6,23 @@ resourcestring
     strAskBuildPaths = 'This command will erase all existing pathnodes and attempt to create a pathnode network on its own.' +
     '  Are you sure this is what you want to do?' + #13#10#13#10 + 'NOTE : This process can take a VERY long time.';
 
+    // Viewport captions/Hints
+    strRealTimePreview = 'RealTime Preview';
+    strWireframe = 'Wireframe';
+    strTextureUsage = 'Texture Usage';
+    strBSPCuts = 'BSP Cuts';
+    strTextured = 'Textured';
+    strDynLight = 'Dynamic Light';
+    strZonesPortals = 'Zone/Portal';
+    strTop = 'Top';
+    strFront = 'Front';
+    strSide = 'Side';
+
+    // Actor class browser
+    strCannotFind = 'Cannot find such class: ';
+    strLoading = 'Loading...';
+
+
 implementation
 
 end.
