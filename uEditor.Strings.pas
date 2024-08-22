42a2cd2444b57b649701a943cd707b978af40dad
LoadLine Calibration <Veronika-and@yandex.ru>
2024-08-23 07:54:42 +0900

    New strings

--- uEditor.Strings.pas
+++ uEditor.Strings.pas
@@ -2,6 +2,11 @@ unit uEditor.Strings;
 
 interface
 
+const
+    NoneString = '(none)';
+    AllString = '(All)';
+
+
 resourcestring
     strAskBuildPaths = 'This command will erase all existing pathnodes and attempt to create a pathnode network on its own.' +
     '  Are you sure this is what you want to do?' + #13#10#13#10 + 'NOTE : This process can take a VERY long time.';
@@ -21,6 +26,11 @@ resourcestring
     // Actor class browser
     strCannotFind = 'Cannot find such class: ';
     strLoading = 'Loading...';
+
+    // Error messages
+    strCannotFindEditPack = 'Can''t find edit package "%s". Cannot continue. Editor will be closed.';
+    strCannotFindEditPackSrc = 'Can''t find edit package "%s", but sources of this package has been found in "%s".' + #13#10#13#10 +
+                               'Rebuild this package and relaunch the editor.';
 
 
 implementation
