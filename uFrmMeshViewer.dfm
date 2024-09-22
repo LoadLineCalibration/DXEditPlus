12e1056ff6316f88b715137fa42d8f106a5c7108
LoadLine Calibration <Veronika-and@yandex.ru>
2024-09-22 18:26:11 +0900

    Implemented mesh viewer/browser

---
+++ uFrmMeshViewer.dfm
@@ -0,0 +1,263 @@
+object frmMeshViewer: TfrmMeshViewer
+  Left = 0
+  Top = 0
+  Caption = 'Mesh Browser'
+  ClientHeight = 480
+  ClientWidth = 498
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poMainFormCenter
+  ShowHint = True
+  OnClose = FormClose
+  OnShow = FormShow
+  TextHeight = 15
+  object MeshViewport: TEsPanel
+    Left = 0
+    Top = 184
+    Width = 498
+    Height = 296
+    Align = alClient
+    TabOrder = 0
+    OnResize = MeshViewportResize
+    FrameStyle = Chess
+    ExplicitLeft = -3
+    ExplicitTop = 187
+    ExplicitWidth = 499
+  end
+  object GroupBox1: TGroupBox
+    AlignWithMargins = True
+    Left = 3
+    Top = 3
+    Width = 492
+    Height = 178
+    Align = alTop
+    Caption = 'Controls'
+    DefaultHeaderFont = False
+    HeaderFont.Charset = DEFAULT_CHARSET
+    HeaderFont.Color = clWindowText
+    HeaderFont.Height = -12
+    HeaderFont.Name = 'Segoe UI'
+    HeaderFont.Style = [fsBold]
+    TabOrder = 1
+    ExplicitWidth = 493
+    object Label1: TLabel
+      AlignWithMargins = True
+      Left = 5
+      Top = 20
+      Width = 80
+      Height = 153
+      Align = alLeft
+      Alignment = taRightJustify
+      AutoSize = False
+      Caption = 'Meshes:'#13#10#13#10'Animations:'#13#10'(Name/Num/'#13#10'NumFrames)'
+      ExplicitTop = 22
+    end
+    object lblFrame: TLabel
+      Left = 167
+      Top = 147
+      Width = 217
+      Height = 25
+      AutoSize = False
+      Caption = 'lblFrame'
+      Layout = tlCenter
+    end
+    object cmbMeshes: TComboBox
+      Left = 91
+      Top = 17
+      Width = 334
+      Height = 22
+      Style = csOwnerDrawFixed
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -12
+      Font.Name = 'Verdana'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 0
+      OnChange = cmbMeshesChange
+    end
+    object lbAnimSeq: TListBox
+      Left = 91
+      Top = 45
+      Width = 293
+      Height = 96
+      Style = lbOwnerDrawFixed
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -12
+      Font.Name = 'Verdana'
+      Font.Style = []
+      Items.Strings = (
+        '123'
+        '123'
+        '5555'
+        '6666'
+        '8888'
+        '99999'
+        '555'
+        '555'
+        '777'
+        '777'
+        '33333')
+      ParentFont = False
+      TabOrder = 1
+      OnClick = lbAnimSeqClick
+      OnDrawItem = lbAnimSeqDrawItem
+    end
+    object btnPrevMesh: TButton
+      Left = 431
+      Top = 17
+      Width = 26
+      Height = 25
+      Caption = '3'
+      Font.Charset = SYMBOL_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -19
+      Font.Name = 'Marlett'
+      Font.Style = [fsBold]
+      ParentFont = False
+      TabOrder = 2
+      OnClick = btnPrevMeshClick
+    end
+    object btnNextMesh: TButton
+      Left = 459
+      Top = 17
+      Width = 26
+      Height = 25
+      Caption = '4'
+      Font.Charset = SYMBOL_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -19
+      Font.Name = 'Marlett'
+      Font.Style = [fsBold]
+      ParentFont = False
+      TabOrder = 3
+      OnClick = btnNextMeshClick
+    end
+    object GroupBox2: TGroupBox
+      Left = 390
+      Top = 45
+      Width = 95
+      Height = 130
+      Caption = 'View'
+      TabOrder = 4
+      object rbTexturedMesh: TRadioButton
+        Left = 8
+        Top = 20
+        Width = 80
+        Height = 17
+        Caption = 'Textured'
+        Checked = True
+        TabOrder = 0
+        TabStop = True
+        OnClick = ChangeViewClick
+      end
+      object rbPolygons: TRadioButton
+        Left = 8
+        Top = 39
+        Width = 80
+        Height = 17
+        Caption = 'Polygons'
+        TabOrder = 1
+        OnClick = ChangeViewClick
+      end
+      object chkAutoDolly: TCheckBox
+        Left = 8
+        Top = 81
+        Width = 81
+        Height = 17
+        Hint = 'Enables/disables model rotation.'
+        Caption = 'Auto dolly'
+        TabOrder = 2
+        OnClick = ChangeViewClick
+      end
+      object rbWireframe: TRadioButton
+        Left = 8
+        Top = 58
+        Width = 80
+        Height = 17
+        Caption = 'Wireframe'
+        TabOrder = 3
+        OnClick = ChangeViewClick
+      end
+      object chkGrayMeshBG: TCheckBox
+        Left = 8
+        Top = 100
+        Width = 80
+        Height = 17
+        Hint = 
+          'Sets the viewport background color to gray. Will be reset defaul' +
+          't to black when the window is closed.'
+        Caption = 'Gray BG'
+        TabOrder = 4
+        OnClick = chkGrayMeshBGClick
+      end
+    end
+    object btnPlayAnim: TButton
+      Left = 91
+      Top = 147
+      Width = 70
+      Height = 25
+      Hint = 'Play/Stop the selected animation'
+      Caption = 'Play >>'
+      TabOrder = 5
+      OnClick = btnPlayAnimClick
+    end
+    object btnZeroFrameAnim: TButton
+      Left = 7
+      Top = 147
+      Width = 26
+      Height = 25
+      Hint = 'Jump to starting frame of selected animation'
+      Caption = '0'
+      TabOrder = 6
+      OnClick = btnZeroFrameAnimClick
+    end
+    object btnNextAnimFlame: TButton
+      Left = 63
+      Top = 147
+      Width = 26
+      Height = 25
+      Hint = 'Next anim frame'
+      Caption = '4'
+      Font.Charset = SYMBOL_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -19
+      Font.Name = 'Marlett'
+      Font.Style = [fsBold]
+      ParentFont = False
+      TabOrder = 7
+      OnClick = btnNextAnimFlameClick
+    end
+    object btnPrevAnimFlame: TButton
+      Left = 35
+      Top = 147
+      Width = 26
+      Height = 25
+      Hint = 'Prev. anim frame'
+      Caption = '3'
+      Font.Charset = SYMBOL_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -19
+      Font.Name = 'Marlett'
+      Font.Style = [fsBold]
+      ParentFont = False
+      TabOrder = 8
+      OnClick = btnPrevAnimFlameClick
+    end
+    object btnRefresh: TButton
+      Left = 7
+      Top = 116
+      Width = 78
+      Height = 25
+      Hint = 'Reloads the list of meshes'
+      Caption = 'Refresh'
+      TabOrder = 9
+      OnClick = btnRefreshClick
+    end
+  end
+end
---
+++ uFrmMeshViewer.pas
@@ -0,0 +1,343 @@
+unit uFrmMeshViewer;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ES.BaseControls, ES.Layouts, Vcl.ExtCtrls,
+  uEditorLoader, System.StrUtils, uEditorTypes, Engine.UnCamera;
+
+type
+  TfrmMeshViewer = class(TForm)
+    MeshViewport: TEsPanel;
+    GroupBox1: TGroupBox;
+    cmbMeshes: TComboBox;
+    Label1: TLabel;
+    lbAnimSeq: TListBox;
+    btnPrevMesh: TButton;
+    btnNextMesh: TButton;
+    GroupBox2: TGroupBox;
+    rbTexturedMesh: TRadioButton;
+    rbPolygons: TRadioButton;
+    chkAutoDolly: TCheckBox;
+    btnPlayAnim: TButton;
+    lblFrame: TLabel;
+    btnZeroFrameAnim: TButton;
+    btnNextAnimFlame: TButton;
+    btnPrevAnimFlame: TButton;
+    btnRefresh: TButton;
+    rbWireframe: TRadioButton;
+    chkGrayMeshBG: TCheckBox;
+
+    // New procedures
+    procedure UpdateMeshList(Cmd: string);
+    procedure UpdateFrameList(Cmd: string);
+    procedure UpdateFrame(Cmd: string);
+    procedure UpdateFrameCaption(Cmd: string);
+    procedure FormShow(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure MeshViewportResize(Sender: TObject);
+    procedure cmbMeshesChange(Sender: TObject);
+    procedure lbAnimSeqDrawItem(Control: TWinControl; Index: Integer; Rect: TRect; State: TOwnerDrawState);
+    procedure btnRefreshClick(Sender: TObject);
+    procedure lbAnimSeqClick(Sender: TObject);
+    procedure btnNextMeshClick(Sender: TObject);
+    procedure btnPrevMeshClick(Sender: TObject);
+    procedure ChangeViewClick(Sender: TObject);
+    procedure btnZeroFrameAnimClick(Sender: TObject);
+    procedure btnPrevAnimFlameClick(Sender: TObject);
+    procedure btnNextAnimFlameClick(Sender: TObject);
+    procedure btnPlayAnimClick(Sender: TObject);
+    procedure chkGrayMeshBGClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMeshViewer: TfrmMeshViewer;
+  FramePos: Integer;
+  NumFrames: Integer;
+  bPlaying: Boolean;
+
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMeshViewer.UpdateMeshList(Cmd: string);
+var
+    ResultStr, S: string;
+begin
+    cmbMeshes.Clear;
+    ResultStr := ServerGetProp('OBJ', 'Query Type=Mesh');
+
+    while True do
+    begin
+        S := ExtractString(ResultStr);
+        if S = '' then Break;
+        cmbMeshes.Items.Add(S);
+    end;
+
+    if cmbMeshes.Items.Count > 0 then
+        cmbMeshes.ItemIndex := 0;
+
+    UpdateFrameList(Cmd);
+end;
+
+procedure TfrmMeshViewer.UpdateFrameList(Cmd: string);
+var
+  i, N: Integer;
+  S: string;
+begin
+    lbAnimSeq.Clear();
+
+    if cmbMeshes.ItemIndex >= 0 then
+    begin
+        N := StrToIntDef(ServerGetProp('MESH', 'NUMANIMSEQS' + ' NAME=' + AnsiQuotedStr(cmbMeshes.Items[cmbMeshes.ItemIndex], '"' )), 0);
+
+        for i := 0 to N - 1 do
+        begin
+            S := Trim(ServerGetProp('MESH', 'ANIMSEQ' + ' NAME=' + AnsiQuotedStr(cmbMeshes.Items[cmbMeshes.ItemIndex], '"') + ' NUM=' + Trim(IntToStr(i))));
+            if S <> '' then
+                lbAnimSeq.Items.Add(S);
+        end;
+        if lbAnimSeq.Count > 0 then
+            lbAnimSeq.ItemIndex := 0;
+    end;
+
+    UpdateFrame(Cmd);
+end;
+
+procedure TfrmMeshViewer.btnNextMeshClick(Sender: TObject);
+begin
+    if cmbMeshes.ItemIndex >= 0 then
+    begin
+        if cmbMeshes.ItemIndex + 1 < cmbMeshes.Items.Count then
+            cmbMeshes.ItemIndex := cmbMeshes.ItemIndex + 1
+        else
+            cmbMeshes.ItemIndex := 0;
+
+        cmbMeshesChange(self);
+    end;
+end;
+
+procedure TfrmMeshViewer.btnNextAnimFlameClick(Sender: TObject);
+begin
+    Inc(FramePos);
+
+    if FramePos >= NumFrames then
+        FramePos := 0;
+
+    UpdateFrameCaption('Update');
+end;
+
+procedure TfrmMeshViewer.btnPlayAnimClick(Sender: TObject);
+begin
+    if bPlaying = True then
+    begin
+        btnPlayAnim.Caption := 'Play >>';
+        btnZeroFrameAnim.Enabled := True;
+        btnPrevAnimFlame.Enabled := True;
+        btnNextAnimFlame.Enabled := True;
+        bPlaying := False;
+    end
+    else
+    begin
+        btnPlayAnim.Caption := 'Stop';
+        btnZeroFrameAnim.Enabled := False;
+        btnPrevAnimFlame.Enabled := False;
+        btnNextAnimFlame.Enabled := False;
+        bPlaying := True;
+    end;
+
+    UpdateFrameCaption('Update');
+end;
+
+procedure TfrmMeshViewer.btnPrevAnimFlameClick(Sender: TObject);
+begin
+    Dec(FramePos);
+
+    if FramePos < 0 then
+        FramePos := NumFrames - 1;
+
+    if FramePos < 0 then
+        FramePos := 0;
+
+    UpdateFrameCaption('Update');
+end;
+
+procedure TfrmMeshViewer.btnPrevMeshClick(Sender: TObject);
+begin
+    if cmbMeshes.ItemIndex >= 0 then
+    begin
+        if cmbMeshes.ItemIndex > 0 then
+            cmbMeshes.ItemIndex := cmbMeshes.ItemIndex - 1
+        else if cmbMeshes.Items.Count > 0 then
+            cmbMeshes.ItemIndex := cmbMeshes.Items.Count - 1;
+
+        cmbMeshesChange(self);
+    end;
+end;
+
+procedure TfrmMeshViewer.btnRefreshClick(Sender: TObject);
+begin
+    UpdateMeshList('Update');
+end;
+
+procedure TfrmMeshViewer.btnZeroFrameAnimClick(Sender: TObject);
+begin
+    FramePos := 0;
+    UpdateFrameCaption('Update');
+end;
+
+procedure TfrmMeshViewer.ChangeViewClick(Sender: TObject);
+begin
+    UpdateFrameCaption('Update');
+end;
+
+procedure TfrmMeshViewer.chkGrayMeshBGClick(Sender: TObject);
+begin
+    case chkGrayMeshBG.Checked of
+        True:  ServerCmd('set editorengine C_WireBackground (R=96,G=96,B=96)');
+        False: ServerCmd('set editorengine C_WireBackground (R=0,G=0,B=0)'); // Reset background color back to default
+    end;
+
+    UpdateFrameCaption('Update');
+end;
+
+procedure TfrmMeshViewer.cmbMeshesChange(Sender: TObject);
+begin
+    UpdateFrameList('Update');
+end;
+
+procedure TfrmMeshViewer.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+    if bPlaying = True then
+        btnPlayAnimClick(self);
+
+    ServerCmd('CAMERA CLOSE NAME=MeshViewCam');
+    ServerCmd('set editorengine C_WireBackground (R=0,G=0,B=0)'); // Reset background color back to default
+    chkGrayMeshBG.Checked := False;
+end;
+
+procedure TfrmMeshViewer.FormShow(Sender: TObject);
+begin
+    UpdateMeshList('Open');
+end;
+
+procedure TfrmMeshViewer.lbAnimSeqClick(Sender: TObject);
+begin
+    UpdateFrame('Update');
+end;
+
+procedure TfrmMeshViewer.lbAnimSeqDrawItem(Control: TWinControl; Index: Integer; Rect: TRect; State: TOwnerDrawState);
+begin
+    var ItemText, NamePart, NumSeqFrames, SeqNum: string;
+    var SpacePos: Integer;
+
+    with (Control as TListBox).Canvas do
+    begin
+        FillRect(Rect);
+
+        // Получить текст элемента
+        ItemText := (Control as TListBox).Items[Index];
+
+        // Найти первый пробел (для разделения названия)
+        SpacePos := Pos(' ', ItemText);
+        if SpacePos > 0 then
+            NamePart := Copy(ItemText, 1, SpacePos - 1)
+        else
+            NamePart := ItemText;
+
+        NumSeqFrames := Copy(ItemText, Length(ItemText) - 2, 3); // Считываем строку справа налево и берём три последних символа.
+        SeqNum := Copy(ItemText, Length(ItemText) - 6, 3); // Продолжаем...
+
+        // Animation sequence name
+        TextOut(Rect.Left + 2, Rect.Top, NamePart);
+
+        // Draw number of sequience frames
+        if NumSeqFrames <> '' then
+            TextOut(Rect.Left + 230, Rect.Top, '[' + NumSeqFrames + ']');
+
+        //
+        if SeqNum <> '' then
+            TextOut(Rect.Left + 190, Rect.Top, '[' + SeqNum + ']');
+    end;
+end;
+
+procedure TfrmMeshViewer.MeshViewportResize(Sender: TObject);
+begin
+    var ViewPortHandle := FindWindowEx(MeshViewport.Handle, 0, 'DXEditPlusUnrealWOpenGLWindowsViewportWindow', nil);
+
+    if ViewPortHandle <> 0 then // The Top should be always 0 in this case
+        SetWindowPos(ViewPortHandle, 0, MeshViewport.Left, 0, MeshViewport.ClientWidth, MeshViewport.ClientHeight,
+                     SWP_NOZORDER or SWP_NOACTIVATE);
+end;
+
+procedure TfrmMeshViewer.UpdateFrame(Cmd: string);
+begin
+    FramePos := 0;
+
+    if lbAnimSeq.Items.Count > 0 then
+        NumFrames := StrToIntDef(Copy(lbAnimSeq.Items[lbAnimSeq.ItemIndex], Length(lbAnimSeq.Items[lbAnimSeq.ItemIndex]) - 2, 3), 0)
+    else
+        NumFrames := 0;
+
+    UpdateFrameCaption(Cmd);
+end;
+
+procedure TfrmMeshViewer.UpdateFrameCaption(Cmd: string);
+var
+    Tmp: string;
+    Flags: Integer;
+begin
+    // Determine temporary string for 'OPEN' command
+    if UpperCase(Cmd) = 'OPEN' then
+        Tmp := ' XR=' + IntToStr(MeshViewport.ClientWidth) + ' YR=' + IntToStr(MeshViewport.ClientHeight)
+    else
+        Tmp := '';
+
+    // Update the label to indicate whether we are playing or displaying a specific frame
+    if bPlaying then
+        lblFrame.Caption := 'Playing ' + IntToStr(NumFrames) + ' frames...'
+    else
+        lblFrame.Caption := 'Frame ' + IntToStr(FramePos) + ' of ' + IntToStr(NumFrames);
+
+    // Check if there are any meshes in the list
+    if cmbMeshes.Items.Count <= 0 then
+    begin
+        ServerCmd('CAMERA CLOSE NAME=MeshViewCam');
+        Exit;
+    end;
+
+    // Build flags for the camera
+    Flags := SHOW_ChildWindow + SHOW_NOBUTTONS;
+
+    if bPlaying then
+        Flags := Flags + SHOW_BACKDROP;
+
+    if bPlaying or chkAutoDolly.Checked then
+        Flags := Flags + SHOW_REALTIME;
+
+    if chkAutoDolly.Checked then
+        Flags := Flags + SHOW_BRUSH;
+
+    if rbPolygons.Checked then
+        Flags := Flags + SHOW_COORDS;
+
+    if rbWireframe.Checked = True then
+        Flags := Flags + SHOW_Frame;
+
+    ServerCmd('CAMERA ' + Cmd + ' NAME=MeshViewCam X=0 Y=0' +
+      ' MESH=' + AnsiQuotedStr(cmbMeshes.Text, '"') +
+      ' REN=' + IntToStr(REN_MESHVIEW) +
+      ' FLAGS=' + IntToStr(Flags) +
+      ' HWND=' + IntToStr(MeshViewport.Handle) +
+      ' MISC1=' + Copy(lbAnimSeq.Items[lbAnimSeq.ItemIndex], Length(lbAnimSeq.Items[lbAnimSeq.ItemIndex]) - 6, 3) +
+      ' MISC2=' + IntToStr(FramePos) + Tmp);
+end;
+
+
+end.
