88908825924234d9522023d09cce34a2cf43a3b5
LoadLine Calibration <Veronika-and@yandex.ru>
2024-09-27 20:53:19 +0900

    Added list of fonts

--- uEditor.Consts.pas
+++ uEditor.Consts.pas
@@ -12,6 +12,12 @@ const EditorPatchFileNames: array[0..5] of string = // To check if Hanfling's Ed
     'RenderExt.int'
 );
 
+const OpenGlDrvFiles: array[0..1] of string = // To check if Hanfling's OpenGlDrv is installed
+(
+    'OpenGlDrv.dll',
+    'OpenGlDrv.int'
+);
+
 const FilterPackages: array[0..8] of string = // Don't show these packages in Actor class browser
 (
     'OpenGLDrv',
@@ -25,8 +31,47 @@ const FilterPackages: array[0..8] of string = // Don't show these packages in Ac
     'RenderExt'
 );
 
+const FilterFonts: array[0..32] of string = // Remove these entries from texture groups
+(
+    'SmallFont', // Engine.u
+    'MedFont',
+    'BigFont',
+    'LargeFont',
+    'TechMedium', // Extension
+    'TechMedium_DS',
+    'TechMedium_B',
+    'TechSmall',
+    'TechSmall_DS',
+    'TechTiny',
+    'FontSansSerif_8_Bold', // DeusExUI
+    'FontSansSerif_8',
+    'FontComputer8x20_C',
+    'FontComputer8x20_A',
+    'FontSpinningDX',
+    'FontComputer8x20_B',
+    'FontLocation',
+    'FontConversationLargeBold',
+    'FontConversationBold',
+    'FontConversation',
+    'FontHUDWingDings',
+    'FontFixedWidthLocation',
+    'FontTitleLarge',
+    'FontMenuSmall_DS',
+    'FontMenuSmall',
+    'FontMenuTitle',
+    'FontMenuHeaders_DS',
+    'FontMenuHeaders',
+    'FontFixedWidthSmall_DS',
+    'FontFixedWidthSmall',
+    'FontMenuExtraLarge',
+    'FontTiny',
+    'FontConversationLarge'
+);
+
 const DXEditPlusIniFile = 'DXEditPlus.ini';
-const MAX_RECENT_FILES = 9; // like in ConEditPlus, actually 10
+const MAX_RECENT_MAPS = 9; // like in ConEditPlus, actually 10
+const GAME_INI_FILE = 'DeusEx.ini'; // Using this file by default
+const ActorBrowserQuickJump = 'ActorBrowser.QuickJump.txt';
 
 
 implementation
